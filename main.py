8a1751a47d78e219420a00d7812f09d40dd0d0d5
bynthytn <80202809+bynthytn@users.noreply.github.com>
2022-01-27 22:10:50 +0300

    serf

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from downloader import download_random_image
 from sorter import sort_image
+from generator import IMAGES_DIR
 
-while True:
-    try:
-        sort_image(download_random_image())
-    except KeyboardInterrupt:
-        exit()
+
+for image in IMAGES_DIR.iterdir():
+    print(image)
+    sort_image(image)
+
+
+# while True:
+#     try:
+#         sort_image(download_random_image())
+#     except KeyboardInterrupt:
+#         exit()
